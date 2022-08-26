b4a3317dc1645370183837e17eea66bfd9e58f4a
bernardio1 <109044709+bernardio1@users.noreply.github.com>
2022-08-26 16:09:40 -0400

    Add files via upload

---
+++ good.js
@@ -0,0 +1,6 @@
+
+
+document.write("<p>I'm current graduate student at the University of Pennsylvania studying computer science with focus on Artificial intelligence.</p>")
+document.write("<p>I believe it's important for people to have role models that look more like them in positions that they aspire to be</p>")
+document.write("<p>I hope to show aspiring engineers that despite the current demographic in STEM fields, they should should not be deterred in their ambitions.</p>")
+document.write("<p>My research interests involve automony and the human-robot interaction</p>")
---
+++ j.js
@@ -0,0 +1,7 @@
+
+
+
+
+var dogAge =window.prompt("enter your dogage");
+var humanAge = (dogAge-2) *4 + 21;
+document.write("your dog is "+ humanAge+ " years old in human years ")
